8b41386ec2475464f7a08a3e0a6e137a7e7fcf69
Ephpha <Ephpha@users.noreply.github.com>
2026-08-10 23:47:28 -0400

    Allow favicon through beta gate

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,6 +29,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|brand/|api/beta-access).*)",
+    "/((?!_next/static|_next/image|favicon.ico|icon.png|brand/|api/beta-access).*)",
   ],
 };
